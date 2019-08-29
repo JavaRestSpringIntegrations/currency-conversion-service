9717ef282a164cf8f5ea28513187a49c3ead1a92
Sunil.Jacob <sunil1405@gmail.com>
2019-08-29 15:53:52 +0530

    Eureka Naming Server

--- src/main/java/com/ivybridge/microservices/CurrencyConversionServiceApplication.java
+++ src/main/java/com/ivybridge/microservices/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.ivybridge.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.ivybridge.microservices")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
